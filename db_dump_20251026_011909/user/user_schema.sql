eabaa36d9b865643446d05d8754c89ac1b6d934b
Jackcybox007 <jackgreengithub@gmail.com>
2025-10-26 02:46:30 +0700

    Implement Market Report and the start of market modeling. #003

--- db_dump_20251026_011909/user/user_schema.sql
+++
@@ -1,9 +0,0 @@
--- table users
-CREATE TABLE users(
-  discord_id TEXT PRIMARY KEY,
-  fio_api_key TEXT NOT NULL,
-  username TEXT,
-  created_at INTEGER NOT NULL,
-  updated_at INTEGER NOT NULL
-);
-
